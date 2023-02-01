d8bbc49fb4ed0750ab96939b22679c4d2db43aa6
Nikitaastapov <nikitaastapov@gmail.com>
2023-02-01 14:48:44 +0300

    var 2

--- books/views.py
+++ books/views.py
@@ -12,9 +12,23 @@ def books_view(request):
 def book_date(request, pub_date):
     template = 'books/books_date.html'
     list_book_filt = Book.objects.filter(pub_date = pub_date)
-    list_book = Book.objects.all()
-    paginator = Paginator(list_book, 1)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {'list_book': page_obj, 'page': page_obj}
+    paginator = Paginator(list_book_filt, 1)
+    # sort_list = Book.objects.order_by('pub_date')
+    
+    previous_page = None 
+    next_page = None
+    def show_date(pub_date, next_page, previous_page):
+        f_d = Book.objects.filter(pub_date__lt= pub_date).values('pub_date').first()
+        if f_d.exists():
+            previous_page = f_d
+        n_d = Book.objects.filter(pub_date__gt=pub_date).values('pub_date').first()
+        if n_d.exists():
+            next_page = n_d
+        
+        
+    
+        
+    
+
+    context = {'list_book': paginator.object_list, 'page': {'up': next_page, 'down': previous_page}}
     return render(request, template, context)
